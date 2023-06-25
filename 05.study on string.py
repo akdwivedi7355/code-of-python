cd0c0da9abc00f0c62996ddc750a7047cee1150a
Aditya Kumar Dwivedi <129512051+akdwivedi7355@users.noreply.github.com>
2023-06-25 13:50:25 +0530

    Add files via upload

---
+++ 05.study on string.py
@@ -0,0 +1,12 @@
+name="aditya"
+number=8947843
+print(name)
+print(name[0:3]) #string print start from 0 and go way till 2 that is (3-1)
+print(name[1:7]) #string print start from 1 and go way till 7 that is (7-1)
+#print(name[a:b]) string print start from a and go way till b-1 that is (b-1)
+print(name.upper())
+print(name.capitalize())
+print(name.lower())
+print(name.count('a')) #it will change first letter of string in upper alphabet
+print(name.isalnum()) # it will check string is only in alpha and numeric not any special character
+print(number.isnumeric())
