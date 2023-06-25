076d970ec84a225b15e7546d83e29ce2c1536db3
Aditya Kumar Dwivedi <129512051+akdwivedi7355@users.noreply.github.com>
2023-06-25 13:49:31 +0530

    Add files via upload

---
+++ 02.variable.py
@@ -0,0 +1,16 @@
+#variable data types
+a=5 #int data type
+print(a)
+b=7
+print(b)
+c='e' #character data type
+ #only e is not consider 
+print(c)
+d=6.4 #float data type
+print(d)
+e="aditya kumar dwivedi" 
+# you can also write 'aditya kumar dwivedi'
+print(e)
+f=None
+ # none is also a data type(doesn't write none)
+print(f)
