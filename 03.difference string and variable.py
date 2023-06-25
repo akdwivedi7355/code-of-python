ff163d6038e6a46fb898decb6822cbc2ac5b69ca
Aditya Kumar Dwivedi <129512051+akdwivedi7355@users.noreply.github.com>
2023-06-25 13:49:47 +0530

    Add files via upload

---
+++ 03.difference string and variable.py
@@ -0,0 +1,13 @@
+#that is valid statement
+a="5"
+print(a)
+#that is also valid statement
+b=5
+print(b)
+'''
+
+but 
+when you perform the operation print(a+1) it is not valid
+onother hand print(b+1) is valid 
+go through another point print(int(a) +1 ) that is also valid statement'''
+print(int(a)+1)
