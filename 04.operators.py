3086f74b0786a439c206c04d74481d66302fdc1f
Aditya Kumar Dwivedi <129512051+akdwivedi7355@users.noreply.github.com>
2023-06-25 13:50:06 +0530

    Add files via upload

---
+++ 04.operators.py
@@ -0,0 +1,45 @@
+#let us talk about operator
+#definition - Operators are used to perform operations on variables and values.
+'''
+Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+'''
+#arithmatic operator
+'''Arithmetic operators are used with 
+   numeric values to perform common
+     mathematical operations:'''
+x=9
+y=4
+
+print("Addition	x + y", x + y)	
+print("Subtraction	x - y", x-y)	
+print("Multiplication	x * y", x*y)	
+print("Division	x / y" ,x/y)	
+print("Modulus	x % y", x%y)	
+print("Exponentiation	x ** y", x**y)	#4 times 9
+print("Floor division	x // y",x//y) #it doesn't show after point value
+
+
+
+#Assignment Operators
+'''Assignment operators are used to assign values to variables'''
+#value of x
+x = 5
+print(x)
+#for increment off value
+x += 3  #it is equal x=x+3
+print(x)
+x-=3 #it is equal x=x-3
+print (x)
+x*=3 #it is equalx=x*3
+print(x)
+x/=3 #it is equal x=x/3
+print(x)
+
